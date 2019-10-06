4deda607930501036c79db90a5ddd10d65753a7c
Aaron Smith <smitham50@gmail.com>
2019-10-06 14:44:36 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelectorAll(selector)[0]
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target')
+}
+
+function deepestChild() {
+  let nodes = document.querySelectorAll('#grand-node div')
+  return nodes[nodes.length - 1]
+}
+
+function increaseRankBy(n) {
+  let list = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < list.length; i++) {
+    let children = list[i].children
+
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
